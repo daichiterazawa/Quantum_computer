dbb7b87403fe293c31346f8ddfea15826d6613fd
daichiterazawa <teratera@opt.comm.eng.osaka-u.ac.jp>
2021-09-08 15:41:26 +0900

    電子波束時間変化アニメーション

--- pulse.animetion.py
+++ pulse.animetion.py
@@ -22,6 +22,7 @@ eV = 1.60217733 * 10**(-19)#電子ボルト
 E0 = 10.0 * eV
 sigma = np.sqrt(np.log(2)) * 10**9
 k0 = np.sqrt(2 * me * E0 /hbar**2) 
+omega = hbar * k0**2 /(2*me)
 #波数
 k = np.linspace(k0 - 5*sigma, k0 + 5*sigma, 400)
 
@@ -30,17 +31,32 @@ k = np.linspace(k0 - 5*sigma, k0 + 5*sigma, 400)
 #シュミレーション開始
 #######################################
 #空間、時間指定
-x_num = 400
+x_num = 200
 x = np.linspace(-5.0 * 10**(-9),5.0 * 10**(-9), x_num)
+t_num = 100
+t = np.linspace(-50 * 10**(-16),50 * 10**(-16), t_num)
 
 #行列表記
-x_matrix,k_matrix = np.meshgrid(x, k)
+x_matrix,t_matrix,k_matrix = np.meshgrid(x, t, k)
 
 
 #波動関数計算
-psi = np.exp(-((k_matrix-k0)/(2*sigma))**2) * np.exp(1j * k_matrix * x_matrix)
-psi = psi.sum(axis=0)
+psi = np.exp(-((k_matrix-k0)/(2*sigma))**2) * np.exp(1j * k_matrix * x_matrix - 1j * hbar * k_matrix**2 / (2*me) * t_matrix)
+psi = psi.sum(axis=2)
 
-plt.plot(x,psi.real)
-plt.plot(x,psi.imag)
+#アニメーションの作成
+fig = plt.figure(figsize = (10,6))
+ims = []
+for i in range(t_num):
+    img = plt.plot(x,psi[i,:].real, color='blue', linewidth=3.0, linestyle='solid')
+    img += plt.plot(x,psi[i,:].imag, color='orange', linewidth=3.0, linestyle='solid')
+    ims.append(img)
+    
+
+#描画
+plt.title("animation")
+plt.xlabel("x")
+plt.ylabel("y")
+anime = animation.ArtistAnimation(fig, ims, interval=10)
+anime.save("pulse.html", writer=animation.HTMLWriter())
 plt.show()
